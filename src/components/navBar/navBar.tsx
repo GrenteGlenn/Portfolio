15fccfc111d7d8078a3c4ad72fabb07a195f068e
kinsuful <grente_g@etna-alternance.net>
2025-05-06 15:38:47 +0200

    [save]

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -64,7 +64,7 @@ const Navbar: FC = () => {
             </Link>
           ))}
           <a
-            href="/images/GlennGrenteCV.pdf"
+            href="/images/GrenteGlennCV.pdf"
             download
             className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors text-sm sm:text-md"
           >
